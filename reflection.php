541726bd121e93fd11fae22c1702f97ece160f66
Adrian Statescu <mergesortv@gmail.com>
2020-01-09 00:31:49 +0200

    Update reflection.php

--- reflection.php
+++ reflection.php
@@ -28,4 +28,21 @@
      var_dump($user);
      echo"</pre>";
 
+class Calc {
+       
+          /**
+           * @param $x The number x
+           * @param %y The number y
+           * @return mixed The number z 
+           */
+           public function sum($x, $y) {
+                  return $x + $y;
+           }
+     } 
+
+     $calc = new Calc();
+     $reflector = new ReflectionClass('Calc');  
+     $comment = $reflector->getMethod('sum')->getDocComment();
+     echo$comment;
+
 ?>
